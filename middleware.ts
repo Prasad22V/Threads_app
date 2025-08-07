06bb87f44bd9ccb53bb566a0f7f248d55a8f7330
Prasad22V <prasadvaidya20818@gmail.com>
2025-08-07 12:37:06 +0530

    ignored error

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,7 @@
+
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware({
-    // An array of public routes that don't require authentication.
-  publicRoutes: ["/api/webhook/clerk"],
-
-  // An array of routes to be ignored by the authentication middleware.
-  ignoredRoutes: ["/api/webhook/clerk"],
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
